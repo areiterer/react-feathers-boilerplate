81125330c3a322d573aecdf84c144bbdf417b0dd
Andreas Reiterer <reiterer.a@gmail.com>
2017-01-28 18:26:20 +0100

    Login: add dummy function for login functionality

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,58 @@
 import React, { Component, PropTypes } from 'react';
 
 class LoginForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			email: '',
+			password: '',
+			rememberMe: false
+		};
+
+		this._handleLogin = this._handleLogin.bind(this);
+		this._handleChange = this._handleChange.bind(this);
+	}
+
+	_handleLogin(event) {
+		event.preventDefault();
+		this.props.onLogin(this.state.email, this.state.password, this.state.rememberMe);
+	}
+
+	_handleChange(event) {
+		if(event.target.type === "checkbox") {
+			this.setState({
+				[event.target.name]: event.target.checked
+			})
+		} else {
+			this.setState({
+				[event.target.name]: event.target.value
+			})
+		}
+	}
+
 	render() {
 		return (
-			<form className="form-signin">
+			<form className="form-signin" onSubmit={this._handleLogin}>
 				<h2 className="form-signin-heading">Please sign in</h2>
-				<label htmlFor="inputEmail" className="sr-only">Email address</label>
-				<input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus/>
-				<label htmlFor="inputPassword" className="sr-only">Password</label>
-				<input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+				<div className="form-group">
+					<label htmlFor="inputEmail" className="sr-only">Email address</label>
+					<input type="email" id="inputEmail" className="form-control"
+					       placeholder="Email address" name="email"
+					       value={this.state.email} onChange={this._handleChange}
+					       required autoFocus/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="inputPassword" className="sr-only">Password</label>
+					<input type="password" id="inputPassword" className="form-control"
+					       placeholder="Password" name="password"
+					       value={this.state.password} onChange={this._handleChange}
+					       required/>
+				</div>
 				<div className="checkbox">
 					<label>
-						<input type="checkbox" value="remember-me"/>Remember me
+						<input type="checkbox" name="rememberMe" checked={this.state.rememberMe}
+						       value="rememberMe" onChange={this._handleChange}/>Remember me
 					</label>
 				</div>
 				<button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,12 +3,22 @@ import React, { Component } from 'react';
 import LoginForm from '../components/LoginForm';
 
 class LoginPage extends Component {
+	constructor(props) {
+		super(props);
+
+		this._login = this._login.bind(this);
+	}
+
+	_login(username, password, rememberMe) {
+		console.log('Logging in: '+ JSON.stringify({username, password, rememberMe}));
+	}
+
 	render() {
 		return (
 			<div className="container">
 				<div className="row">
 					<div className="col-xs-offset-4 col-xs-4">
-						<LoginForm />
+						<LoginForm onLogin={this._login} />
 					</div>
 				</div>
 			</div>
