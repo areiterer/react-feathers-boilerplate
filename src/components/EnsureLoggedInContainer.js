e2cbc4cb6e8f50bbbb9305cfe731a6980244f2a1
Andreas Reiterer <reiterer.a@gmail.com>
2017-02-17 19:42:29 +0100

    Fix: if protected link is visited directly you got referred to login even if you are already logged in.

--- src/components/EnsureLoggedInContainer.js
+++ src/components/EnsureLoggedInContainer.js
@@ -2,12 +2,17 @@ import { Component } from 'react';
 import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 
+import {app} from '../lib/WebApi';
 
 class EnsureLoggedInContainer extends Component {
 	componentDidMount() {
-		if (!this.props.isLoggedIn) {
-			browserHistory.replace('/login');
-		}
+		app.authenticate().then(() =>
+				this.props.setLoginState(true)
+			)
+			.catch(error => {
+				if (error.code === 401)
+					browserHistory.replace('/login');
+			})
 	}
 
 	render() {
